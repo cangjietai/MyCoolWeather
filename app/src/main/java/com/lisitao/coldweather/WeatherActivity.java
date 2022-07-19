1de39fc6d08ad29518d0476d95ba827552b3640b
cangjietai <1296172553@qq.com>
2022-07-19 23:54:38 +0800

    将状态栏设置为透明

--- app/src/main/java/com/lisitao/coldweather/WeatherActivity.java
+++ app/src/main/java/com/lisitao/coldweather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package com.lisitao.coldweather;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
@@ -61,6 +63,17 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 改变系统UI
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            // 修改系统UI的显示，活动的布局会显示到状态栏上
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            // 将状态栏设为透明色
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+
         setContentView(R.layout.activity_weather);
         // 初始化各控件
         bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
